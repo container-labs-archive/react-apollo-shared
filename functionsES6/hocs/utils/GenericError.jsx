6c08051599257b39201dce8ba89515f34507d9b8
Will Beebe <will@containerlabs.io>
2018-08-02 15:01:02 -0700

    upping core an react

--- functionsES6/hocs/utils/GenericError.jsx
+++ functionsES6/hocs/utils/GenericError.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Error from '@material-ui/icons/Error';
+import { withStyles } from '@material-ui/core/styles';
 
-const CONTAINER_STYLES = {
-  textAlign: 'center',
-  marginTop: '20px',
+const styles = {
+  wrapper: {
+    textAlign: 'center',
+    marginTop: '20px',
+  },
 };
 
+@withStyles(styles)
 export default function GenericError(props) {
   const {
+    classes,
     error, // eslint-disable-line
   } = props;
 
   return (
-    <div style={CONTAINER_STYLES}>
+    <div className={classes.wrapper}>
       <Error />
     </div>
   );
--- functionsES6/hocs/utils/Loader.jsx
+++ functionsES6/hocs/utils/Loader.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
 
-const LOADING_STYLES = {
-  textAlign: 'center',
-  marginTop: '20px',
+const styles = {
+  wrapper: {
+    textAlign: 'center',
+    marginTop: '20px',
+  },
 };
 
+@withStyles(sytles)
 export default function Loader() {
+  const { classes } = this.props;
+
   return (
-    <div style={LOADING_STYLES}>
+    <div className={classes.wrapper}>
       <CircularProgress />
     </div>
   );
